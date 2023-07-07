487db37f9caa93cd7934d41fb221da6d5e615bb7
Husnain Ali <103342056+Husnain1Ali@users.noreply.github.com>
2023-07-07 05:45:33 -0400

    Add files via upload

---
+++ Question_1/Question_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");  //Here we are  printing  the Hello Word
+            Console.ReadKey();    //Here we stop the code 
+        }
+    }
+}
