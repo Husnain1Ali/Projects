566c252fa491d3182c8dc571bb1b0815ca1094df
Husnain Ali <103342056+Husnain1Ali@users.noreply.github.com>
2023-07-07 05:49:17 -0400

    Add files via upload

---
+++ Question_2/Question_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter your Name");                // Here we are asking to user to put his/her name
+            string user_name = Console.ReadLine();              // Here we are getting the name a nd assign to variable (user_name)
+            Console.WriteLine("Hello, " + user_name);          // Here we concatenate the Hello a nd User name
+            Console.ReadKey();                                // Here we are stoping the code
+        }
+    }
+}
