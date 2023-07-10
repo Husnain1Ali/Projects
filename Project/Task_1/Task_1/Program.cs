af89d5eac06ab6586bc44ff3ab799ed96b547a45
Husnain Ali <103342056+Husnain1Ali@users.noreply.github.com>
2023-07-10 06:42:56 -0400

    Add files via upload
    Task_(1)

---
+++ Project/Task_1/Task_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SentenceComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the sentence:");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(' ');
+
+            Dictionary<string, int> wordFrequencies = new Dictionary<string, int>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string currentWord = words[i].ToLower();
+
+                if (wordFrequencies.ContainsKey(currentWord))
+                {
+                    wordFrequencies[currentWord]++;
+                }
+                else
+                {
+                    wordFrequencies[currentWord] = 1;
+                }
+            }
+
+            foreach (var kvp in wordFrequencies)
+            {
+                Console.WriteLine($"The word '{kvp.Key}' appears {kvp.Value} time(s) in the sentence.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
