09f05c0096506c5ad876f9e24b985b5845abc194
Husnain Ali <103342056+Husnain1Ali@users.noreply.github.com>
2023-07-10 06:44:09 -0400

    Add files via upload
    task_6

---
+++ Project/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the sentence:");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(' ');
+
+            foreach (string word in words)
+            {
+                int vowelCount = 0;
+                int consonantCount = 0;
+
+                foreach (char letter in word.ToLower())
+                {
+                    if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+                    {
+                        vowelCount++;
+                    }
+                    else if (char.IsLetter(letter))
+                    {
+                        consonantCount++;
+                    }
+                }
+
+                Console.WriteLine("Word: " + word);
+                Console.WriteLine("Vowels: " + vowelCount);
+                Console.WriteLine("Consonants: " + consonantCount);
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
