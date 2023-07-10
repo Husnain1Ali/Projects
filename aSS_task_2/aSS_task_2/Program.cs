8feb20452c2f38932b2bf3e8bc87f201be69fe63
Husnain Ali <103342056+Husnain1Ali@users.noreply.github.com>
2023-07-10 16:06:00 -0400

    Add files via upload

---
+++ aSS_task_2/aSS_task_2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class BankAccount
+{
+    public int Account_No;
+    public string Acoount_Holder_Name;
+    public int Balance;
+    
+    public BankAccount(int account_no, string acoount_Holder_Name, int balance)
+    {   Account_No = account_no;
+        Acoount_Holder_Name = acoount_Holder_Name;
+        Balance = balance;
+        
+    }
+    public virtual void Desposit_Money()
+    {
+        
+        Console.WriteLine($"The Account number {Account_No} whose owner name is {Acoount_Holder_Name} deposit money");
+    }
+    public virtual void WithDraw_Moneny()
+    {
+        Console.WriteLine($"The Account number {Account_No} whose owner name is {Acoount_Holder_Name} WithraW_Money money");
+    }
+    public virtual void Display_Infomation()
+    {
+        Console.WriteLine($"The Account number {Account_No} whose owner name is {Acoount_Holder_Name} has balance {Balance}");
+    }
+}
+
+public class Saving_Account:BankAccount
+{
+    public Saving_Account(int account_no, string account_Holder_Name, int balance ,int interest_rate) : base(account_no, account_Holder_Name, balance)
+    {
+    }
+
+    public override void Desposit_Money()
+    {
+        int interest;
+        
+        int Deposite;
+        interest = (Deposite / Balance) * 100;
+        Balance += interest;
+        Console.WriteLine($"The balance aftert interest is {Balance}");
+    }
+
+}
+
+
+
+
+
+
+
+
+namespace aSS_task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
